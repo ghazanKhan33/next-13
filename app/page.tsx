18dca9546320bdd1d4cc9b14cb5de62f52a8dadc
ghazanKhan33 <ghazan.elitx@gmail.com>
2023-10-31 13:00:48 +0500

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+    <h1 className='border-red-500 p-4 max-w-sm mx-auto text-center'>Hello world</h1>
+    </main>
+  )
+}
