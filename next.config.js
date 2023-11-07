10576e130975cc4551bce09d46f90c1a9cfb9de4
ghazanKhan33 <ghazan.elitx@gmail.com>
2023-11-07 14:18:20 +0500

    Changed the config file again

--- next.config.js
+++ next.config.js
@@ -1,71 +1,73 @@
-const { sources } = require('next/dist/compiled/webpack/webpack');
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'standalone',
-    forwardingGateway: {
-      requiredHeaders: {
-        'X-Azure-FDID': 'ADDED_DURING_DEPLOYMENT',
-      },
-      allowedForwardedHosts: [
-        'ADDED_DURING_DEPLOYMENT',
-        'app.dakotasoft.com',
-        'www.app.dakotasoft.com',
-      ],
-    },
-    generateEtags: false,
+module.exports = {
     async headers() {
-        return [
-          {
-            source: '/(.*).json',
-            headers: [
-              {
-                key: 'Cache-Control',
-                value: 'no-cache',
-              },
-            ],
-          },
-          {
-            source: '/static/(.*)',
-            headers: [
-              {
-                key: 'Cache-Control',
-                value: 'public, max-age=31536000, immutable',
-              },
-            ],
-          },
-          {
-            source: '/(logo.png|favicon.(png|jpg|ico))',
-            headers: [
-              {
-                key: 'Cache-Control',
-                value: 'public, max-age=31536000, immutable',
-              },
-            ],
-          },
-          {
-            source: '/(.*)',
-            headers: [
-              {
-                key: 'ETag',
-                value: 'DynamicallyGenerated',
-              },
-              {
-                key: 'Cache-Control',
-                value: 'private, no-cache, no-store, must-revalidate',
-              },
-              {
-                key: 'pragma',
-                value: 'no-cache',
-              },
-              {
-                key: 'expires',
-                value: 'Sat, 01 Jan 2000 00:00:00 GMT',
-              },
-            ],
-          },
-        ];
-      },
+      return [
+        {
+          source: '/(.*)', // Match all routes
+          headers: [
+            {
+              key: 'ETag',
+              value: 'DynamicallyGenerated',
+            },
+            {
+              key: 'Cache-Control',
+              value: 'public,max-age=31536000,immutable',
+            },
+          ],
+        },
+        {
+          source: '/.*\\.json',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'no-cache',
+            },
+          ],
+        },
+        {
+          source: '/static/.*\\.(css|js|png|jpg|gif|svg|map)',
+          // No specific headers defined for this route
+        },
+        {
+          source: '/logo.png',
+          // No specific headers defined for this route
+        },
+        {
+          source: '/favicon.(png|jpg|ico)',
+          // No specific headers defined for this route
+        },
+      ];
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/(.*)',
+          destination: '/index.html',
+        },
+      ];
+    },
+    async redirects() {
+      return [
+        {
+          source: '/401',
+          destination: '/login',
+          statusCode: 302,
+        },
+      ];
+    },
+    async navigationFallback() {
+      return {
+        rewrite: '/index.html',
+        exclude: [
+          '/static/css/(.*)',
+          '/static/media/.*\\.(png|jpg|gif|svg)',
+          '/static/js/(.*)',
+        ],
+      };
+    },
+    async mimeTypes() {
+      return {
+        '.json': 'text/json',
+      };
+    },
   };
-
-module.exports = nextConfig
+  
