1270b7442eef759d8b99d9b88a2d9d9723bbfe2a
ghazanKhan33 <ghazan.elitx@gmail.com>
2023-11-10 00:57:33 +0500

    back to next file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 module.exports = {
-
-  };
-  
+  headers: {
+    "ETag": "DynamicallyGenerated",
+    "Cache-Control": "public,max-age=31536000,immutable"
+  },
+  rewrites: [
+    {
+      source: "/static/*.{css,js,png,jpg,gif,svg,map}",
+      destination: "/static/:path*"
+    },
+    {
+      source: "/logo.png",
+      destination: "/logo.png"
+    },
+    {
+      source: "/favicon.{png,jpg,ico}",
+      destination: "/favicon/:path*"
+    }
+  ],
+  redirects: [
+    {
+      source: "/*",
+      destination: "/index.html",
+      exclude: ["/static/css/*", "/static/media/*.{png,jpg,gif,svg}", "/static/js/*"]
+    }
+  ],
+  mimeTypes: {
+    ".json": "text/json"
+  }
+};
