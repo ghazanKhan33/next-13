1c4ede585b4f4422af6b130994f3fbc4ccec706c
ghazanKhan33 <ghazan.elitx@gmail.com>
2023-11-10 01:54:37 +0500

    Changed it again

--- next.config.js
+++ next.config.js
@@ -1,26 +1,24 @@
 module.exports = {
-  async rewrites(){ 
+  async rewrites() {
     return [
       {
         source: "/static/:path*.{css,js,png,jpg,gif,svg,map}",
         destination: "/static/:path*"
       },
       {
-        source: "/logo.png",
-        destination: "/logo.png"
-      },
-      {
         source: "/favicon.{png,jpg,ico}",
         destination: "/favicon/:file*"
       }
-  ]},
+    ];
+  },
   async redirects() {
-    return[
-    {
-      source: "/*",
-      destination: "/index.html",
-      exclude: ["/static/css/*", "/static/media/*.{png,jpg,gif,svg}", "/static/js/*"]
-    }
-  ]},
+    return [
+      {
+        source: "/*",
+        destination: "/index.html",
+        exclude: ["/static/css/*", "/static/media/*.{png,jpg,gif,svg}", "/static/js/*"]
+      }
+    ];
+  },
   generateEtags: false,
 };
