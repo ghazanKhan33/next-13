5daaa7ea5e4a31e1bd40d920b0e4b00220e849bd
ghazanKhan33 <ghazan.elitx@gmail.com>
2023-11-14 19:03:12 +0500

    changed the config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: process.env.REACT_APP_SERVER_HOST,
+    port: parseInt(process.env.REACT_APP_SERVER_PORT, 10),
+    // https: {
+    //   key: path.resolve(__dirname, process.env.REACT_APP_SERVER_HTTPS_KEY),
+    //   cert: path.resolve(__dirname, process.env.REACT_APP_SERVER_HTTPS_CERT),
+    // },
+    origin:process.env.REACT_APP_SERVER_ORIGIN,
+  },
+  build: {
+    sourcemap: process.env.REACT_APP_BUILD_SOURCEMAP === 'true',
+  },
 })
