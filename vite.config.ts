23b797de8b006995298c4c99090af2f82bbde908
ghazanKhan33 <ghazan.elitx@gmail.com>
2023-11-14 20:40:07 +0500

    changed env and config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: process.env.REACT_APP_SERVER_HOST,
-    port: parseInt(process.env.REACT_APP_SERVER_PORT, 10),
-    // https: {
-    //   key: path.resolve(__dirname, process.env.REACT_APP_SERVER_HTTPS_KEY),
-    //   cert: path.resolve(__dirname, process.env.REACT_APP_SERVER_HTTPS_CERT),
-    // },
-    origin:process.env.REACT_APP_SERVER_ORIGIN,
-  },
-  build: {
-    sourcemap: process.env.REACT_APP_BUILD_SOURCEMAP === 'true',
-  },
+export default defineConfig(({mode})=>{
+ const env = loadEnv(mode, process.cwd(),'')
+
+ return (
+  {
+  
+    plugins: [react()],
+    server: {
+      host: env.REACT_APP_SERVER_HOST,
+      port: parseInt(env.VITE_REACT_APP_SERVER_PORT),
+      // https: {
+      //   key: path.resolve(__dirname, process.env.REACT_APP_SERVER_HTTPS_KEY),
+      //   cert: path.resolve(__dirname, process.env.REACT_APP_SERVER_HTTPS_CERT),
+      // },
+      origin:env.REACT_APP_SERVER_ORIGIN,
+    },
+    build: {
+      sourcemap: env.REACT_APP_BUILD_SOURCEMAP === 'true',
+    },
+  }
+ )
 })
